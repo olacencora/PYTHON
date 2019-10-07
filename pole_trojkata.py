7372e8be8305088d7d8784a9656da6fb57bfd503
ola <olacencora@gmail.com>
2019-10-07 21:01:56 +0200

    first function - areafunction -working

--- pole_trojkata.py
+++ pole_trojkata.py
@@ -1,4 +1,34 @@
-print("podaj pierwszą daną a = ")
-a = input()
-print("a")
+import math
+
+def areafunction( a, b, c ):
+    perimeter = float
+    perimeter = a + b + c
+
+    print("Obwód trójkąta to: " + str( perimeter ))
+
+    p = float
+    p = perimeter / 2
+
+    print("połowa obdwodu trójkąta to:" + str(p))
+
+    z = float
+    z = (p * (p - a) * (p - b) * (p - c))
+
+    print("składowa wyniku pola to:" + str(z))
+
+    area = float
+    area = math.sqrt(z)
+
+    return(print("pole trójkąta to:" + str(area)))
+
+
+a = float (input("podaj pierwszą zmienną a = "))
+b = float (input("podaj drugą zmienną b = "))
+c = float (input("podaj trzecią zmienną c = "))
+
+if (a <= 0) or (b <= 0) or (c <=0 ):
+    print("podana wartość jest zła")
+else:
+    areafunction(a, b, c)
+
 

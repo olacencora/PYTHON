915bbe89c14cdacf3f0bf99603ce41fbaacae3c2
ola <olacencora@gmail.com>
2019-10-08 22:19:25 +0200

    task finished

--- pole_trojkata.py
+++ pole_trojkata.py
@@ -1,6 +1,6 @@
 import math
 
-def areafunction( a, b, c ):
+def area_calculate( a, b, c ):
     perimeter = float
     perimeter = a + b + c
 
@@ -19,16 +19,34 @@ def areafunction( a, b, c ):
     area = float
     area = math.sqrt(z)
 
-    return(print("pole trójkąta to:" + str(area)))
+    return(print("Pole trójkąta to:" + str(area)))
+
+
+def check_triangle_angle( a, b, c):
+    if math.pow( a,2 ) + math.pow( b,2 ) == math.pow( c,2 ) or math.pow( c,2 ) + math.pow( b,2 ) == math.pow( a,2 ) or math.pow( c,2 ) + math.pow( a,2 ) == math.pow( b,2 ):
+        print("trójkąt jest prostokątny")
+    elif math.pow( a,2 ) + math.pow( b,2 ) < math.pow( c,2 ) or math.pow( c,2 ) + math.pow( b,2 ) < math.pow( a,2 ) or math.pow( c,2 ) + math.pow( a,2 ) < math.pow( b,2 ):
+        print("trójkąt jest rozwartokątny")
+    else:
+        print("trójkąt jest ostrokątny")
+
+def check_triangle_side( a, b, c ):
+    if a == b == c:
+        print("trójkąt jest równoboczny")
+    elif a == b or a == c or c == b:
+        print("trójką jest równoramienny")
+    else:
+        print("trójkąt jest różnoboczny")
 
 
 a = float (input("podaj pierwszą zmienną a = "))
 b = float (input("podaj drugą zmienną b = "))
 c = float (input("podaj trzecią zmienną c = "))
 
-if (a <= 0) or (b <= 0) or (c <=0 ):
-    print("podana wartość jest zła")
+if (a > 0) and (b > 0) and (c > 0) and (a + b > c) and (a + c > b) and (b + c > a):
+    area_calculate(a, b, c)
+    check_triangle_angle(a, b, c)
+    check_triangle_side(a, b, c)
 else:
-    areafunction(a, b, c)
-
+    print("podane boki nie spełniają warunku zbudowania trójkąta")
 

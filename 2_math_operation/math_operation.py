aa04178c2dc1c2b80fcfb1e0b50de6ab4d842ca9
ola <olacencora@gmail.com>
2019-10-18 00:10:14 +0200

    almost there

--- 2_math_operation/math_operation.py
+++ 2_math_operation/math_operation.py
@@ -14,11 +14,10 @@ print(no_whitespaces)
 
 #metodą list comprehension wrzucam kazda litere stringa do listy
 list_equation = [i for i in no_whitespaces]
-print(list_equation)
+print("wyrażenie w liście to: " + str(list_equation))
 
 # sprawdzam na której pozycji jest "=" w równaniu
 equal_position = list_equation.index("=")
-print(equal_position)
 float(equal_position)
 print(type(equal_position))
 
@@ -38,10 +37,9 @@ for i in list_equation:
         left_side.append(i)
     else:
         right_side.append(i)
-print(left_side)
-print(right_side)
+print("lewa strona równania to: " + str(left_side))
+print("prawa strona równania to: " + str(right_side))
 
-list_equation[1]
 # sprawdzam liczbę przed x
 x_position = list_equation.index("x")
 print(x_position)
@@ -49,31 +47,60 @@ if x_position == 0:
     numb_before_x = 1
 else:
     numb_before_x = list_equation[x_position - 1]
+print("liczba przed x to: " + str(numb_before_x))
 
 # funkcja sprawdzająca liczbę obok x
+
 def check_numb_next_x_right():
-    for i in list_equation_right:
+    for i in right_side:
         if (i != "x") and (i != numb_before_x) and (i != "+") and (i != "-"):
-            numb_next_x = list_equation_right[i]
+            numb_next_x = i
         else:
             numb_next_x = 0
+    print("x znajduje sie po prawej stronie i wynosi:" + str(numb_next_x))
     return numb_next_x
 
 def check_numb_next_x_left():
-    for i in list_equation_left:
+    for i in left_side:
         if (i != "x") and (i != numb_before_x) and (i != "+") and (i != "-"):
-            numb_next_x = list_equation_left[i]
+            numb_next_x = i
         else:
             numb_next_x = 0
+    print("x znajduje sie po lewej stronie a liczba obok wynosi: " + str(numb_next_x))
     return numb_next_x
 
-# sprawdzam po ktorej stronie wyrazenia jest "x" i wywołuje funkcję check_numb_next_x()
+# sprawdzam po ktorej stronie wyrazenia jest "x" i wywołuje funkcję zanajdującą liczbe obok x
+int = numb_next_x = 0
 for i in left_side:
     if (i == "x"):
-        check_numb_next_x()
+        check_numb_next_x_left()
+        numb_opposite_x = right_side[0]
     else:
-        for i in list_equation_right:
+        for i in right_side:
             if (i == "x"):
-                check_numb_next_x()
+                check_numb_next_x_right()
+                numb_opposite_x = left_side[0]
+print("liczba po przeciwnej stronie rownania do x to: " + str(numb_opposite_x))
 
-# sprawdzam liczbe
+#sprawdzam znak przed numb next to x
+numb_next_x_position = list_equation.index(numb_next_x)
+if numb_next_x == 0:
+    sign_numb_before_x = ""
+else:
+    sign_numb_next_x = list_equation[numb_next_x - 1]
+print("znak przed liczba obok x to: " + str(sign_numb_before_x))
+
+# zmieniam str na int
+
+int(numb_before_x)
+int(numb_opposite_x)
+int(numb_next_x)
+
+# wyliczam x
+if sign_numb_before_x == "":
+    find_numb = numb_opposite_x / numb_next_x
+elif sign_numb_before_x == "+":
+    find_numb = (numb_opposite_x - numb_next_x) / numb_next_x
+else:
+    find_numb = (numb_opposite_x + numb_next_x) / numb_next_x
+print("x =" + str(find_numb))
